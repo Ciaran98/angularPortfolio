c429951ebf0493e2a78dd917bcc7b044a3f59e5a
Ciaran98 <ciaran.melarkey@hotmail.com>
2022-04-19 19:20:22 +0100

    Changed more CSS and Boostrap, added more animation to the about me page

--- src/app/project-display/project-display.component.ts
+++ src/app/project-display/project-display.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { GetDBContentService } from '../get-db-content.service';
 import { Title } from '@angular/platform-browser';
 import projectjson from 'src/assets/project.json'
 @Component({
@@ -10,19 +9,15 @@ import projectjson from 'src/assets/project.json'
 })
 export class ProjectDisplayComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute,private db : GetDBContentService, private titleService: Title) { }
+  constructor(private route: ActivatedRoute, private titleService: Title) { }
   public id: string ="";
   public title: string="";
   public projecttext: string="";
+  public projecttitle: string="";
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id')!;
-    this.db.getProjectFromID(this.id).subscribe(
-      res =>{
-        this.title = res[0][1];
-        this.titleService.setTitle(res[0][1]);
-      }
-    )
     this.projecttext = projectjson[parseInt(this.id.replace(":",""))-1]["projectdata"]["projecttext"]
+    this.projecttitle = projectjson[parseInt(this.id.replace(":",""))-1]["projectname"]
   }
 
 }
