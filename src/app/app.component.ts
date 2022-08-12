427ab8a704e8eea46e4748631a76240a5e7c0b6f
Ciaran98 <ciaran.melarkey@hotmail.com>
2022-08-12 12:24:42 +0100

    Added new projects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,27 @@ import {Title} from '@angular/platform-browser';
 import { GetDBContentService } from './get-db-content.service';
 import { Router,ActivatedRoute } from '@angular/router';
 import { AccountService } from './account.service';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3kuFcPPIm_HomHydUiqrEWhfvC6eF7oc",
+  authDomain: "ciaran-melarkey.firebaseapp.com",
+  projectId: "ciaran-melarkey",
+  storageBucket: "ciaran-melarkey.appspot.com",
+  messagingSenderId: "609002675982",
+  appId: "1:609002675982:web:dec3d0c49d19213728815f",
+  measurementId: "G-0R0VNQPML2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
